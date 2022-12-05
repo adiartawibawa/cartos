615081c25ec53827177e7cc99881df272cd79e8a
adiartawibawa <surat.buat.adi@gmail.com>
2022-12-05 16:01:53 +0800

    adding publishing system

--- resources/js/Components/Navbars.vue
+++ resources/js/Components/Navbars.vue
@@ -2,33 +2,68 @@
     <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
         <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div class="relative flex h-16 items-center justify-between">
-                <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                <div
+                    class="absolute inset-y-0 left-0 flex items-center sm:hidden"
+                >
                     <!-- Mobile menu button-->
                     <DisclosureButton
-                        class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                        class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                    >
                         <span class="sr-only">Open main menu</span>
-                        <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
-                        <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
+                        <Bars3Icon
+                            v-if="!open"
+                            class="block h-6 w-6"
+                            aria-hidden="true"
+                        />
+                        <XMarkIcon
+                            v-else
+                            class="block h-6 w-6"
+                            aria-hidden="true"
+                        />
                     </DisclosureButton>
                 </div>
-                <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div
+                    class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start"
+                >
                     <div class="flex flex-shrink-0 items-center">
-                        <img class="block h-8 w-auto lg:hidden"
-                            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
-                        <img class="hidden h-8 w-auto lg:block"
-                            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
+                        <img
+                            class="block h-8 w-auto lg:hidden"
+                            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                            alt="Your Company"
+                        />
+                        <img
+                            class="hidden h-8 w-auto lg:block"
+                            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                            alt="Your Company"
+                        />
                     </div>
                     <div class="hidden sm:ml-6 sm:block">
                         <div class="flex space-x-4">
-                            <a v-for="item in navigation" :key="item.name" :href="item.href"
-                                :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'px-3 py-2 rounded-md text-sm font-medium']"
-                                :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
+                            <a
+                                v-for="item in navigation"
+                                :key="item.name"
+                                :href="item.href"
+                                :class="[
+                                    item.current
+                                        ? 'bg-gray-900 text-white'
+                                        : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                    'px-3 py-2 rounded-md text-sm font-medium',
+                                ]"
+                                :aria-current="
+                                    item.current ? 'page' : undefined
+                                "
+                                >{{ item.name }}</a
+                            >
                         </div>
                     </div>
                 </div>
-                <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                    <button type="button"
-                        class="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                <div
+                    class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
+                >
+                    <button
+                        type="button"
+                        class="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                    >
                         <span class="sr-only">View notifications</span>
                         <BellIcon class="h-6 w-6" aria-hidden="true" />
                     </button>
@@ -37,30 +72,53 @@
                     <Menu as="div" class="relative ml-3">
                         <div>
                             <MenuButton
-                                class="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                                class="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                            >
                                 <span class="sr-only">Open user menu</span>
-                                <img class="h-8 w-8 rounded-full" :src="user.imageUrl" alt="" />
+                                <img
+                                    class="h-8 w-8 rounded-full"
+                                    :src="user.imageUrl"
+                                    alt=""
+                                />
                             </MenuButton>
                         </div>
-                        <transition enter-active-class="transition ease-out duration-100"
+                        <transition
+                            enter-active-class="transition ease-out duration-100"
                             enter-from-class="transform opacity-0 scale-95"
                             enter-to-class="transform opacity-100 scale-100"
                             leave-active-class="transition ease-in duration-75"
                             leave-from-class="transform opacity-100 scale-100"
-                            leave-to-class="transform opacity-0 scale-95">
+                            leave-to-class="transform opacity-0 scale-95"
+                        >
                             <MenuItems
-                                class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-
-                                <MenuItem v-for="item in userNavigation" :key="item.name" v-slot="{ active }">
-                                <a :href="item.href"
-                                    :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">{{
-                                            item.name
-                                    }}</a>
+                                class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                            >
+                                <MenuItem
+                                    v-for="item in userNavigation"
+                                    :key="item.name"
+                                    v-slot="{ active }"
+                                >
+                                    <a
+                                        :href="item.href"
+                                        :class="[
+                                            active ? 'bg-gray-100' : '',
+                                            'block px-4 py-2 text-sm text-gray-700',
+                                        ]"
+                                        >{{ item.name }}</a
+                                    >
                                 </MenuItem>
                                 <MenuItem v-slot="{ active }">
-                                <Link :href="route('logout')" method="post" as="button"
-                                    :class="[active ? 'bg-gray-100' : '', 'flex w-full px-4 py-2 text-sm text-gray-700']">
-                                Sign Out</Link>
+                                    <Link
+                                        :href="route('logout')"
+                                        method="post"
+                                        as="button"
+                                        :class="[
+                                            active ? 'bg-gray-100' : '',
+                                            'flex w-full px-4 py-2 text-sm text-gray-700',
+                                        ]"
+                                    >
+                                        Sign Out</Link
+                                    >
                                 </MenuItem>
                             </MenuItems>
                         </transition>
@@ -71,35 +129,52 @@
 
         <DisclosurePanel class="sm:hidden">
             <div class="space-y-1 px-2 pt-2 pb-3">
-                <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href"
-                    :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block px-3 py-2 rounded-md text-base font-medium']"
-                    :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
+                <DisclosureButton
+                    v-for="item in navigation"
+                    :key="item.name"
+                    as="a"
+                    :href="item.href"
+                    :class="[
+                        item.current
+                            ? 'bg-gray-900 text-white'
+                            : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                        'block px-3 py-2 rounded-md text-base font-medium',
+                    ]"
+                    :aria-current="item.current ? 'page' : undefined"
+                    >{{ item.name }}</DisclosureButton
+                >
             </div>
         </DisclosurePanel>
     </Disclosure>
-
 </template>
 
 <script setup>
-import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline'
-import { usePage, Link } from '@inertiajs/inertia-vue3'
+import {
+    Disclosure,
+    DisclosureButton,
+    DisclosurePanel,
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuItems,
+} from "@headlessui/vue";
+import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/vue/24/outline";
+import { usePage, Link } from "@inertiajs/inertia-vue3";
 
 const user = {
     name: usePage().props.value.auth.user.name,
     email: usePage().props.value.auth.user.email,
-    imageUrl:
-        'http://0.gravatar.com/avatar/d358e30c77a4329e81fea9c7b81d6b96',
-}
+    imageUrl: "http://0.gravatar.com/avatar/d358e30c77a4329e81fea9c7b81d6b96",
+};
 const navigation = [
-    { name: 'Dashboard', href: '#', current: true },
-    { name: 'Blogs', href: '#', current: false },
-    { name: 'Projects', href: '#', current: false },
-    { name: 'Courses', href: '#', current: false },
+    { name: "Dashboard", href: "#", current: false },
+    { name: "Blogs", href: "/blogger", current: false },
+    { name: "Projects", href: "#", current: false },
+    { name: "Courses", href: "#", current: false },
     // { name: 'Reports', href: '#', current: false },
-]
+];
 const userNavigation = [
-    { name: 'Your Profile', href: 'profile' },
-    { name: 'Settings', href: 'settings' }
-]
+    { name: "Your Profile", href: "profile" },
+    { name: "Settings", href: "settings" },
+];
 </script>
